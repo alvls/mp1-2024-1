91564c119674ca31209e46a1315379ad7f83e500
elus10n <dizbvalkyre@gmail.com>
2024-03-04 20:28:53 +0300

    no interface(

--- lukin.ia/task2/task2.cpp
+++ lukin.ia/task2/task2.cpp
@@ -7,39 +7,12 @@ class Matrix
 	int** matr;
 	int size = 0;
 public:
-	Matrix()
-	{
-		cout << "Please, enter the matrix size! It must be 2-8!" << endl;
-		for (int flag = 0; flag == 0;)
-		{
-			cin >> size;
-			if (size < 2 || size > 8)
-				cout << "Incorrect size!See the instructions!" << endl;
-			else 
-				flag = 1;
-		}
-		cout << endl;
-		matr = new int* [size];
-		for (int i = 0; i < size; i++)
-			matr[i] = new int[size];
-		for (int i = 0; i < size; i++)
-			for (int j = 0; j < size; j++)
-				matr[i][j] = 0;
-	}
 	Matrix(int size_)
-	{ 
+	{
 		if (size_ < 2 || size_ > 8)
-		{
-			cout << "Incorrect size!Enter size manually!It must be 2-8!" << endl;
-			for (int i = 0; i == 0;)
-			{
-				cin >> size_;
-				if (size_ < 2 || size_ > 8)
-					cout << "Incorrect size!See the instructions!" << endl;
-				else i = 1;
-			}
-		}
-		size = size_;
+			size = 2;
+		else
+			size = size_;
 		matr = new int* [size];
 		for (int i = 0; i < size; i++)
 			matr[i] = new int[size];
@@ -59,44 +32,30 @@ public:
 	}
 	void print_matrix()
 	{
-		cout << "Your matrix:" << endl;
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++)
 				cout << matr[i][j] << '\t';
-			cout << endl<<endl<<endl<<endl;
+			cout << endl << endl << endl << endl;
 		}
-		cout << endl << endl;
+		cout << endl<< endl;
 	}
 	int GetSize()
 	{
 		return size;
 	}
-	void SetElement()
+	void SetElement(int cols, int rows, int value)
 	{
-		int i, j, value;
-		print_matrix();
-		cout << "Please, enter the indexes of the elememt, that you want to set!" << endl;
-		for (int flag = 0; flag == 0;)
-		{
-			cin >> i >> j;
-			if (i<1 || i>size || j<1 || j>size)
-				cout << "Incorrect indexes. Check the size!" << endl;
-			else
-				flag = 1;
-		}
-		i = i - 1;
-		j = j - 1;
-		cout << "Now, enter the value of the element!" << endl;
-		cin >> value;
-		matr[i][j] = value;
+		if (cols < 1 || cols > size || rows < 1 || rows > size)
+			return;
+		else 
+			matr[cols - 1][rows - 1] = value;
 	}
-	int GetElement()
+	int GetElement(int cols, int rows)
 	{
-		int i, j;
-		cout << "Please, enter the indexes of the elememt, that you want to get!" << endl;
-		cin >> i >> j;
-		return matr[i][j];
+		if (cols < 1 || cols > size || rows < 1 || rows > size)
+			return matr[0][0];
+		return matr[cols - 1][rows - 1];
 	}
 	bool diag_predominance()
 	{
@@ -129,7 +88,6 @@ public:
 	{
 		if (size != term.size)
 		{
-			cout << "The matrix sizes must be equal! The original matrix was returned" << endl;
 			return *this;
 		}
 		Matrix result(*this);
@@ -145,7 +103,6 @@ public:
 	{
 		if (this->size != term.size)
 		{
-			cout << "The matrix sizes must be equal! The left matrix was returned" << endl;
 			return *this;
 		}
 		Matrix result(*this);
@@ -157,14 +114,14 @@ public:
 				result.matr[i][j] = matr[i][j] + term.matr[i][j];
 		return result;
 	}
-	Matrix& operator=(const Matrix& term)//это написал, чтобы сложение нормально работало
+	Matrix& operator=(const Matrix& term)
 	{
 		if (this == &term)
 			return *this;
 		for (int i = 0; i < this->size; i++)
 			delete[] this->matr[i];
 		delete[] this->matr;
-		this->size = term.size;//по-хорошему здесь надо проверять, выделилась ли память, но так как есть проверки на корректный размер, то вроде всегда будет правильно выделяться
+		this->size = term.size;
 		matr = new int* [this->size];
 		for (int i = 0; i < this->size; i++)
 			matr[i] = new int[this->size];
@@ -184,19 +141,22 @@ public:
 int main()
 {
 	srand(time(NULL));
-	//тест всех конструкторов, ех4 показывает реакцию конструктора на неверные параметры.
-	Matrix ex1;
+	//тест конструкторов + тест на поведение при неверных значениях
+	Matrix ex1(4), ex2(1), ex3(9);
+	ex1.print_matrix();
+	ex2.print_matrix();
+	ex3.print_matrix();
+	//тест геттера + метод с рандом()
 	ex1.filling_rand_values();
 	ex1.print_matrix();
-	Matrix ex2(5), ex3(ex1), ex4(1);
-	cout << "This is the ex3 - copy of ex1" << endl;
-	ex3.print_matrix();
-	//тест геттера и сеттера
-	cout <<"ex1 size is: " << ex1.GetSize() << endl<<endl;
-	ex1.SetElement();
+	cout <<"ex 1 size = "<< ex1.GetSize() << endl <<"ex1 elem on pos 2,3 = "<< ex1.GetElement(2, 3) << endl <<"reaction for incorrect pos is return elem on pos 1, 1 = "<< ex1.GetElement(5, 3) << endl << endl;
+	//тест сеттера
+	cout << "setter test" << endl << endl;
+	ex1.SetElement(2, 2, 100);
 	ex1.print_matrix();
-	//тест опрератора сложения + присваивания
-	Matrix term1(3), term2(3), term_incorrect(4), result(6);
+	//тест сложения в нескольких вариантах
+	cout << "addition test" << endl << endl;
+	Matrix term1(3), term2(3), term_incorrect(4), result(6);//показано, что будет, если сложить матрицы разных размеров + тест перегрузки присваивания
 	term1.filling_rand_values();
 	term2.filling_rand_values();
 	term_incorrect.filling_rand_values();
@@ -204,9 +164,11 @@ int main()
 	term2.print_matrix();
 	result = term1 + term2;
 	result.print_matrix();
+	cout << "reaction on incorrect addition, lhf was returned" << endl << endl;
 	result = term1 + term_incorrect;
 	result.print_matrix();
-	//тест диаг. преобладания
+	//это диагональное преобладание
+	cout << "diag_predominance test" << endl << endl;
 	Matrix test(3);
 	for (int flag = 0; flag == 0;)
 	{
