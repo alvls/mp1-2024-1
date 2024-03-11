e5ad157f33f1ee8d4ba30fff2d65dc0199edec9a
LeoGNZV <leogonz2005@mail.ru>
2024-03-11 14:00:57 +0300

    Исправлено задание №2

--- gonozov_la/task2/Source.cpp
+++ gonozov_la/task2/Source.cpp
@@ -5,10 +5,14 @@ using namespace std;
 class Din_array {
 	size_t sz;
 	double* array;
-	size_t ssz;
-	double* subarray = new double[1];
+	Din_array(size_t sz_, double* pr_array) {
+		sz = sz_ / 2;
+		array = new double[sz];
+		for (size_t index{}; index < sz; index++)
+			array[index] = pr_array[index * 2 + 1];
+	}
 public:
-	Din_array(size_t sz_, double default_) {
+	Din_array(double default_, size_t sz_) {
 		sz = sz_;
 		array = new double[sz] {default_};
 	}
@@ -36,34 +40,28 @@ public:
 		}
 		return true;
 	}
-	void create_subarray() {
-		ssz = sz / 2;
-		delete[] subarray;
-		subarray = new double[ssz];
-		for (size_t index{}; index < ssz; index++)
-			subarray[index] = array[index * 2 + 1];
+	Din_array create_subarray() {
+		return Din_array(sz, array);
 	}
 	void output_a() {
 		cout << "Размер массива:" << sz <<endl;
 		for (size_t index{}; index < sz; index++)
 			cout << array[index]<< " ";
 	}
-	void output_sa() {
+	/*void output_sa() {
 		cout << "Размер подмассива:" << ssz << endl;
 		for (size_t index{}; index < ssz; index++)
 			cout << subarray[index]<< " ";
-	}
+	}*/
 	~Din_array() {
 		delete [] array;
-		delete [] subarray;
 		array = nullptr;
-		subarray = nullptr;
 	}
 };
 
 void main() {
 	setlocale(LC_ALL, "rus");
-	Din_array s(15, 0); //создание объекта
+	Din_array s(0, 15); //создание объекта
 	size_t t = s.find(); // используется функция, возвращающая размер массива
 	cout << t << endl;
 	s.set_element(3, 1); // функция: по индексу 1 динамического массива записать 3
@@ -72,9 +70,11 @@ void main() {
 	cout << s.sorted_check() << endl; //проверка на отсортированность
 	s.set_element(-1.25, 2); 
 	cout << s.find_min_element() << endl; //нахождение минимального элемента
-	s.create_subarray(); // создание подмассива по нечетным индексам
+	Din_array c = s.create_subarray(); // создание подмассива по нечетным индексам
 	s.output_a(); // вывод информации о массиве
 	cout << "\n";
-	s.output_sa(); // вывод информации о опдмассиве
+	c.output_a(); // вывод информации о массиве
+	cout << "\n";
+	//s.output_sa(); // вывод информации о опдмассиве
 	system("pause");
 }
