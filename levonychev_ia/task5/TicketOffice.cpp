59c79a517fea69199b4c0ab966b164df929ee8ec
fartekool <levonychev2006@gmail.com>
2024-04-23 14:12:03 +0300

    Обновление class TicketOffice и вспомогательных struct

--- levonychev_ia/task5/TicketOffice.cpp
+++ levonychev_ia/task5/TicketOffice.cpp
@@ -1,24 +1,8 @@
-
-//Каждый сеанс описывается датой, временем начала сеанса, названием фильма, номером зала, стоимостью билета в зависимости от зоны(VIP и обычная).
-//Для упрощения считать, что покупатель указывает тип зоны и требуемое число билетов, а места выделяются кассой автоматически(при наличии свободных).
-//Зрительные места в каждом зале описываются номером ряда и номером в ряду.
-//Продажа билетов на сеанс прекращается через 10 минут после начала сеанса.
-//Информация о всех сеансах на ближайшие 30 дней проката хранится в классе Кинотеатр.
-//Для каждого зала установлена базовая стоимость билетов(на дневные сеансы – от 12.00 до 18.00).
-//Стоимость билетов на утренние сеансы(до 12.00) составляет 75 % от базовой, стоимость билетов на вечерние сеансы(после 18.00) – 150 % от базовой.
-//Информация о зрительных местах(свободно / занято) в каждом зале на каждом сеансе также хранится в классе Кинотеатр.
-// 
-//Класс Билетная касса должен предоставлять следующие операции: 
-//1) принять данные покупателя : дату, время сеанса, название фильма, номер зала, тип зоны, число мест, 
-//2) проверить наличие требуемого количества свободных мест в требуемой зоне, 
-//3) зарезервировать требуемое количество мест, 
-//4) рассчитать общую стоимость билетов, 
-//5) отменить заказ билетов, 
-//6) сформировать билеты(каждый билет включает : дату, время сеанса, название фильма, номер зала, номер ряда, номер места в ряду).
 #include "TicketOffice.h"
 #include <string>
 #include <iostream>
 #include <ciso646>
+#include <ctime>
 TicketOffice::TicketOffice(Cinema& cinema_) : cinema(cinema_) {}
 double TicketOffice::buy_tickets(Date date, Time time, std::string name, unsigned hall_number, unsigned seat_type, unsigned count_of_tickets)
 {	
@@ -43,8 +27,28 @@ double TicketOffice::buy_tickets(Date date, Time time, std::string name, unsigne
 	{
 		throw std::invalid_argument("Error!");
 	}
+	srand(std::time(NULL));
 	if (count_of_tickets > movie_session_ptr->free_seats)
 		throw std::invalid_argument("Error!");
+	for (int i = 0; i < count_of_tickets; i++)
+	{	
+		int flag = 1;
+		int seat_number;
+		do
+		{
+			seat_number = 1 + rand() % (hall_ptr->row_count * hall_ptr->seat_count);
+			for (int k : movie_session_ptr->occupied_seats)
+			{
+				if (k == seat_number)
+				{
+					flag = 0;
+					break;
+				}
+				flag = 1;
+			}
+		} while (flag == 0);
+		movie_session_ptr->occupied_seats.push_back(seat_number);
+	}
 	price = movie_session_ptr->base_cost * count_of_tickets;
 	if (seat_type == 1)
 		price *= 2;
@@ -53,6 +57,53 @@ double TicketOffice::buy_tickets(Date date, Time time, std::string name, unsigne
 	if (time.hour > 18)
 		price *= 1.5;
 	movie_session_ptr->free_seats -= count_of_tickets;
+	std::cout << "----TICKET----" << std::endl;
+	std::cout << "Movie: \"" << name << "\"" << std::endl;
+	std::cout << "Date: " << date << std::endl;
+	std::cout << "Time: " << time << std::endl;
+	std::cout << "Hall " << hall_ptr->hall_number << std::endl;
+	std::cout << "Count of tickets: " << count_of_tickets << std::endl;
+	std::cout << "Type: ";
+	if (seat_type == 0)
+		std::cout << "base" << std::endl;
+	if (seat_type == 1)
+		std::cout << "VIP" << std::endl;
+	int size = movie_session_ptr->occupied_seats.size();
+	for (int i = 1; i <= count_of_tickets; i++)
+	{	
+		int row_number;
+		int seat_number;
+		if (movie_session_ptr->occupied_seats[size - i] % hall_ptr->seat_count == 0)
+		{
+			row_number = movie_session_ptr->occupied_seats[size - i] / (hall_ptr->seat_count);
+			seat_number = 15;
+		}
+		else
+		{
+			row_number = movie_session_ptr->occupied_seats[size - i] / (hall_ptr->seat_count) + 1;
+			seat_number = movie_session_ptr->occupied_seats[size - i] % hall_ptr->seat_count;
+		}
+		
+		std::cout << i << ") " << "Row " << row_number << ", Seat " << seat_number << std::endl;
+	}
+	std::cout << "----" << std::endl;
+	std::cout << "TOTAL PRICE: " << price << std::endl;
+	std::cout << "----" << std::endl << std::endl;
 	return price;
 
+}
+void TicketOffice::cancel_order(MovieSession& a, unsigned count)
+{	
+	int flag = 0;
+	for (MovieSession& b : cinema.movie_sessions)
+	{
+		if (a == b)
+		{
+			for (int i = 1; i <= count; i++)
+				b.occupied_seats.pop_back();
+			flag = 1;
+		}
+	}
+	if (flag == 0)
+		throw std::invalid_argument("Error!");
 }
