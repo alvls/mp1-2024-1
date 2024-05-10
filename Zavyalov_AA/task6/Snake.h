0d52d2f49921476eab9f2748a7819c95874ca2da
H1nkk <the_h1nk@mail.ru>
2024-05-10 17:34:36 +0300

    Snake done but laggy

--- Zavyalov_AA/task6/Snake.h
+++ Zavyalov_AA/task6/Snake.h
@@ -2,14 +2,217 @@
 #include <Windows.h>
 #include <iostream>
 #include <vector>
+#include <random>
+#include <conio.h>
+#include <string>
+#include <time.h>
+
+#define KB_LEFT 37
+#define KB_UP 38
+#define KB_RIGHT 39
+#define KB_DOWN 40
 
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+void end_animation(short n, short m, char c) {
+	if (c == 'w') {
+		while (true) {
+			for (int i = 1; i < 15; i++) {
+				SetConsoleCursorPosition(hConsole, { short(n + 4), m });
+				SetConsoleTextAttribute(hConsole, i);
+				std::cout << "You won!";
+				Sleep(150);
+			}
+
+		}
+	}
+	else if (c == 'l')
+	{
+		SetConsoleCursorPosition(hConsole, { short(n + 4), m });
+		SetConsoleTextAttribute(hConsole, 15);
+		std::cout << "You lost.";
+	}
+}
+bool is_restricted(COORD pos, std::vector<COORD> restricted_positions) {
+	for (COORD coord : restricted_positions) {
+		if (pos.X == coord.X && pos.Y == coord.Y) {
+			return true;
+		}
+	}
+	return false;
+}
+bool wait_for_key(int timeout_milliseconds, WORD& ch, int& timeleft) {
+	HANDLE tui_handle = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD tui_evtc = 0;
+	DWORD curtime = GetTickCount();
+	DWORD deadline = curtime + timeout_milliseconds;
+	INPUT_RECORD tui_inrec = { 0 };
+	DWORD tui_numread = 0;
+
+	while (GetTickCount() < deadline) {
+		if (tui_evtc > 0) {
+			ReadConsoleInput(tui_handle, &tui_inrec, 1, &tui_numread);
+			if (tui_inrec.EventType == KEY_EVENT) {
+				if (tui_inrec.Event.KeyEvent.bKeyDown) {
+					ch = tui_inrec.Event.KeyEvent.wVirtualKeyCode;
+					timeleft = deadline - GetTickCount();
+					return true;
+				}
+			}
+		}
+		YieldProcessor();
+		GetNumberOfConsoleInputEvents(tui_handle, &tui_evtc);
+	}
+	timeleft = 0;
+	return false;
+}
 class Snake
 {
 	COORD head;
 	std::vector<COORD> body;
+	int size = 5;
 public:
-
+	Snake() {};
+	Snake(COORD headpos) : head(headpos) {}
+	void Show() {
+		SetConsoleTextAttribute(hConsole, 4);
+		SetConsoleCursorPosition(hConsole, head);
+		std::cout << char(219);
+		SetConsoleTextAttribute(hConsole, 2);
+		for (short i = 1; i <= 4; i++) {
+			SetConsoleTextAttribute(hConsole, 2);
+			COORD pos = { head.X + i, head.Y };
+			SetConsoleCursorPosition(hConsole, pos);
+			std::cout << char(219);
+			body.push_back(pos);
+		}
+	}
+	void Hide() {
+		SetConsoleCursorPosition(hConsole, head);
+		std::cout << char(32);
+		for (COORD x : body) {
+			SetConsoleCursorPosition(hConsole, x);
+			std::cout << char(32);
+		}
+	}
+	bool Move(WORD direction) {
+		if (direction == KB_LEFT) {
+			for (auto element : body) {
+				if ((element.X == head.X - 1) && (element.Y == head.Y)) {
+					return false;
+				}
+			}
+			std::vector<COORD> new_body(size - 1);
+			new_body[0] = head;
+			head = { short(head.X - 1), head.Y };
+			for (int i = 1; i < size - 1; i++) {
+				new_body[i] = body[i - 1];
+			}
+			SetConsoleCursorPosition(hConsole, body[size - 2]);
+			std::cout << char(32);
+			SetConsoleTextAttribute(hConsole, 2);
+			SetConsoleCursorPosition(hConsole, new_body[0]);
+			std::cout << char(219);
+			SetConsoleTextAttribute(hConsole, 4);
+			SetConsoleCursorPosition(hConsole, head);
+			std::cout << char(219);
+			body = new_body;
+			return true;
+		}
+		if (direction == KB_RIGHT) {
+			for (auto element : body) {
+				if ((element.X == head.X + 1) && (element.Y == head.Y)) {
+					return false;
+				}
+			}
+			std::vector<COORD> new_body(size - 1);
+			new_body[0] = head;
+			head = { short(head.X + 1), head.Y };
+			for (int i = 1; i < size - 1; i++) {
+				new_body[i] = body[i - 1];
+			}
+			SetConsoleCursorPosition(hConsole, body[size - 2]);
+			std::cout << char(32);
+			SetConsoleTextAttribute(hConsole, 2);
+			SetConsoleCursorPosition(hConsole, new_body[0]);
+			std::cout << char(219);
+			SetConsoleTextAttribute(hConsole, 4);
+			SetConsoleCursorPosition(hConsole, head);
+			std::cout << char(219);
+			body = new_body;
+			return true;
+		}
+		if (direction == KB_UP) {
+			for (auto element : body) {
+				if ((element.X == head.X) && (element.Y == head.Y - 1)) {
+					return false;
+				}
+			}
+			std::vector<COORD> new_body(size - 1);
+			new_body[0] = head;
+			head = { head.X, short(head.Y - 1)};
+			for (int i = 1; i < size - 1; i++) {
+				new_body[i] = body[i - 1];
+			}
+			SetConsoleCursorPosition(hConsole, body[size - 2]);
+			std::cout << char(32);
+			SetConsoleTextAttribute(hConsole, 2);
+			SetConsoleCursorPosition(hConsole, new_body[0]);
+			std::cout << char(219);
+			SetConsoleTextAttribute(hConsole, 4);
+			SetConsoleCursorPosition(hConsole, head);
+			std::cout << char(219);
+			body = new_body;
+			return true;
+		}
+		if (direction == KB_DOWN) {
+			for (auto element : body) {
+				if ((element.X == head.X) && (element.Y == head.Y + 1)) {
+					return false;
+				}
+			}
+			std::vector<COORD> new_body(size - 1);
+			new_body[0] = head;
+			head = { head.X, short(head.Y + 1) };
+			for (int i = 1; i < size - 1; i++) {
+				new_body[i] = body[i - 1];
+			}
+			SetConsoleCursorPosition(hConsole, body[size - 2]);
+			std::cout << char(32);
+			SetConsoleTextAttribute(hConsole, 2);
+			SetConsoleCursorPosition(hConsole, new_body[0]);
+			std::cout << char(219);
+			SetConsoleTextAttribute(hConsole, 4);
+			SetConsoleCursorPosition(hConsole, head);
+			std::cout << char(219);
+			body = new_body;
+			
+			return true;
+		}
+	}
+	void Eat(COORD foodpos) {
+		std::vector<COORD> new_body(size);
+		new_body[0] = head;
+		head = foodpos;
+		for (int i = 1; i < size; i++) {
+			new_body[i] = body[i - 1];
+		}
+		body = new_body;
+		SetConsoleTextAttribute(hConsole, 2);
+		SetConsoleCursorPosition(hConsole, body[0]);
+		std::cout << char(219);
+		SetConsoleTextAttribute(hConsole, 4);
+		SetConsoleCursorPosition(hConsole, head);
+		std::cout << char(219);
+		size++;
+	}
+	COORD getHead() { // для class game, для проверки на то можно ли ехать куда-то (не стена ли там)
+		return head;
+	}
+	std::vector<COORD> getBody() { // для проверки на то, можно ли создать еду в клетке
+		return body;
+	}
+	int getSize() { return size; }
 };
 
 class Field
@@ -64,7 +267,7 @@ public:
 	Food(COORD pos) {
 		position = pos;
 	}
-	void Show(int color = 12) {
+	void Show(int color = 6) {
 		SetConsoleCursorPosition(hConsole, position);
 		SetConsoleTextAttribute(hConsole, color);
 		std::cout << char(219);
@@ -78,5 +281,159 @@ public:
 
 class SnakeGame
 {
+	Snake snake;
+	Field field;
+	short width;
+	short height;
+	int speed; // period between tacts in milliseconds
+	int goal;
+public:
+	SnakeGame(short n, short m, int goal_, int speed_ = 500): field(n, m), width(m), height(n), goal(goal_), speed(speed_){}
+	void Play() {
+		std::srand(std::time(NULL));
+		short headx = 2 + rand() % (width - 6), heady = 2 + rand() % (height - 2);
+		snake = Snake({ headx, heady });
+		field.Show();
+		snake.Show();
+		WORD KB_code = KB_LEFT, prev_KB_code = KB_LEFT;
+		bool isFoodEaten = true;
+		bool win = true;
+		COORD foodpos = { 0,0 };
+		while (true) {
+			
+			/*std::vector<COORD> restricted = snake.getBody();
+			restricted.push_back(snake.getHead());
+			COORD foodpos = restricted[0];
+			while (is_restricted(foodpos, restricted)) {
+				short foodx = 1 + rand() % width, foody = 1 + rand() % height;
+				foodpos = { foodx, foody };
+			}
+			Food food(foodpos);*/
+			DWORD curtime = GetTickCount();
+			if (isFoodEaten) {
+				std::vector<COORD> restricted = snake.getBody();
+				restricted.push_back(snake.getHead());
+				foodpos = restricted[0];
+				while (is_restricted(foodpos, restricted)) {
+					short foodx = 1 + rand() % width, foody = 1 + rand() % height;
+					foodpos = { foodx, foody };
+				}
+				Food food(foodpos);
+				food.Show();
+				isFoodEaten = false;
+			}
+			int timeleft;
+			//DWORD curtime = GetTickCount();
+			wait_for_key(speed, KB_code, timeleft);
+			//DWORD curtime = GetTickCount();
 
+			switch (KB_code)
+			{
+			case KB_LEFT:
+				if (prev_KB_code == KB_RIGHT) {
+					KB_code = KB_RIGHT;
+				}
+				break;
+
+			case KB_RIGHT:
+				if (prev_KB_code == KB_LEFT) {
+					KB_code = KB_LEFT;
+				}              
+				break;
+
+			case KB_UP:
+				if (prev_KB_code == KB_DOWN) {
+					KB_code = KB_DOWN;
+				}               
+				break;
+
+			case KB_DOWN:
+				if (prev_KB_code == KB_UP) {
+					KB_code = KB_UP;
+				}
+				break;
+
+			}
+
+			COORD nextheadpos;
+
+			switch (KB_code)
+			{
+			case KB_LEFT:
+				if (snake.getHead().X - 1 < 1) {
+					win = false;
+				}
+				else
+				{
+					if (foodpos.X == snake.getHead().X - 1 && foodpos.Y == snake.getHead().Y) {
+						snake.Eat(foodpos);
+						isFoodEaten = true;
+					}
+				}
+				break;
+			case KB_RIGHT:
+				if (snake.getHead().X + 1 > width) {
+					win = false;
+				}
+				else
+				{
+					if (foodpos.X == snake.getHead().X + 1 && foodpos.Y == snake.getHead().Y) {
+						snake.Eat(foodpos);
+						isFoodEaten = true;
+					}
+				}
+				break;
+			case KB_UP:
+				if (snake.getHead().Y - 1 < 1) {
+					win = false;
+				}
+				else
+				{
+					if (foodpos.X == snake.getHead().X && foodpos.Y == snake.getHead().Y - 1) {
+						snake.Eat(foodpos);
+						isFoodEaten = true;
+					}
+				}
+				break;
+			case KB_DOWN:
+				if (snake.getHead().Y + 1 > height) {
+					win = false;
+				}
+				else
+				{
+					if (foodpos.X == snake.getHead().X && foodpos.Y == snake.getHead().Y + 1) {
+						snake.Eat(foodpos);
+						isFoodEaten = true;
+					}
+				}
+				break;
+			default:
+				break;
+			}
+
+			if (win == false) break;
+			snake.Move(KB_code);
+			prev_KB_code = KB_code;
+
+			if (snake.getSize() == goal) {
+				break;
+			}
+
+			while (DWORD ds = GetTickCount() < curtime + timeleft) {}
+			std::cin.clear();
+			// hod igroka
+			//Sleep(500);
+		}
+		if (win) {
+			end_animation(height, width, 'w');
+		}
+		else
+		{
+			end_animation(height, width, 'l');
+		}
+	}
+	void End() {
+		field.Hide();
+		snake.Hide();
+	}
 };
--- Zavyalov_AA/task6/Source.cpp
+++ Zavyalov_AA/task6/Source.cpp
@@ -5,34 +5,43 @@ using namespace std;
 
 int main() {
 	//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 12);
-	std::cout << "Hello world!";
-	SetConsoleTextAttribute(hConsole, 13);
-	std::cout << "Hello world!";
+
 	SetConsoleTextAttribute(hConsole, 15);
-
-	/*for (int i = 3; i < 6; i++) {
-		for (int j = 3; j < 6; j++) {
-			COORD cord = { i, j };
-			Food f = Food(cord);
-			f.Show();
-			Sleep(1000);
-			f.Hide();
-		}
-	}*/
-
 	//for (int i = 0; i < 254; i++) {
 	//	cout << i << ' ' << char(i) << endl;
 	//}
-
-	Field f(5, 9);
+	/*endAnimation(15, 20, 'l');
+	Snake s({ 5, 8 });
+	s.Show();
+	Sleep(500);
+	s.Move('l');
+	Sleep(500);
+	s.Move('u');
+	Sleep(500);
+	s.Move('r');
+	Sleep(500);
+	s.Move('d');
+	Sleep(500);
+	s.Eat({ s.getHead().X, short(s.getHead().Y - 1) });
+	Sleep(500);
+	Field f(15, 20);
 	f.Show();
 	Sleep(1000);
 	Food fo({ 7, 4 });
 	fo.Show();
 	Sleep(1000);
-	f.Hide();
+	f.Hide();*/
+	SnakeGame sg(15, 20, 6);
+	sg.Play();
+	WORD s;
+	int t;
+	while (true) {
+		wait_for_key(500, s, t);
+		if (s != 37)
+			cout << "gsd " << t << endl;
+	}
 	system("pause");
+
 
 	return 0;
 }
