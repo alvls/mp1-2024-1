142b81b62c686d320026b0be6908712de4cc24ad
H1nkk <the_h1nk@mail.ru>
2024-05-11 18:43:40 +0300

    Task 6 done

--- Zavyalov_AA/task6/Source.cpp
+++ Zavyalov_AA/task6/Source.cpp
@@ -1,12 +1,10 @@
-#include <iostream>
 #include "Snake.h"
 
 using namespace std;
 
 int main() {
-	//HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	while (true) {
-		SnakeGame sg(15, 20, 7, 300);
+		SnakeGame sg(15, 20, 8, 150);
 		sg.Play();
 	}
 	system("pause");
