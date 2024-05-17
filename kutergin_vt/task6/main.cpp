9765053d38ae771f518ad046c2e80b7098352960
VALancaster <valentin.kutergin@gmail.com>
2024-05-17 16:51:45 +0300

    developed the Snake game

--- kutergin_vt/task6/main.cpp
+++ kutergin_vt/task6/main.cpp
@@ -1,74 +1,277 @@
-#include "file_credit.h"
-#include "file_processing_center.h"
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    try {
-        Credit CRDT;
-        Processing_Center PC;
-        // заполним базу клиентов
-        PC.createAccount(2343, "Иванов Иван Иванович", "qwerty123", 200000);
-        PC.createAccount(3857, "Валентинов Валентин Валентинович", "123456789", 2500000);
-        PC.createAccount(9999, "Андреев Андрей Андреевич", "secret_password999", 1000000);
-        PC.createAccount(0001, "Ильин Илья Ильич", "1990_Ilyin_Ilya", 4050000);
-        // заполним базу кредитов
-        Credit CR1(1, 100000.0, 12.0);
-        Credit CR2(1, 450000.0, 15.0);
-        Credit CR3(2, 750000.0, 15.0);
-        Credit CR4(3, 800000.0, 12.0);
-        Credit CR5(3, 1000000.0, 18.0);
-        Credit CR6(5, 1500000.0, 20.0);
-        Credit CR7(5, 2000000.0, 22.0);
-        Credit CR8(15, 3000000.0, 24.0);
-        PC.addCreditToClient(3857, CR4);
-        PC.AddCredit(CR1);
-        PC.AddCredit(CR2);
-        PC.AddCredit(CR3);
-        PC.AddCredit(CR4);
-        PC.AddCredit(CR5);
-        PC.AddCredit(CR6);
-        PC.AddCredit(CR7);
-        PC.AddCredit(CR8);
-        
-        cout << "Welcome to Internet Bank" << endl;
-        cout << "Please enter your account number: " << endl;
-        int acc_num;
-        cin >> acc_num;
-        cout << "Please enter your password: " << endl;
-        string password;
-        cin >> password;
-        if (!CRDT.authorize(acc_num, password, PC))
-            throw invalid_argument("Authorization error");
-        
-        CRDT.showINFO(acc_num, PC);
-        
-        cout << "Select a credit from the Available credits menu (enter index):" << endl;
-        int choice;
-        cin >> choice;
-        if (!CRDT.checkCreditExistence(acc_num, PC) && CRDT.checkCreditAvailability(acc_num, PC, choice)) // если у клиента нет кредитов и выбранный кредит пользователю доступен
+class Field
+{
+public:
+    int width; // ширина поля
+    int height; // высота поля
+    char** cells; // массив клеток
+
+    // ---------------------------------------------------------------------------------
+
+    Field(int width2, int height2) // конструктор-инициализатор
+    {
+        width = width2 + 2; // добавление стен
+        height = height2 + 2; // добавление стен
+        // выделение памяти под двумерный массив
+        cells = new char* [height];
+        for (int i = 0; i < height; i++)
+            cells[i] = new char[width]; // выделение памяти под новый одномерный массив
+        // инициализация всех клеток пустыми
+        memset(cells[0], '#', width); // верхняя граница
+        for (int i = 1; i < height - 1; i++)
         {
-            CRDT.getCredit(acc_num, PC, choice);
-            cout << "You have a credit " << choice << " approved" << endl;
+            memset(cells[i], ' ', width); // заполнение пробелами
+            cells[i][0] = '#'; // левая граница
+            cells[i][width - 1] = '#'; // правая граница
         }
-        else
-            throw runtime_error("You can't get credit");
-        CRDT.showCreditState(acc_num, PC);
-        cout << "What part of the credit would you like to pay? " << endl;
-        cout << "Enter the payment amount: " << endl;
-        double payment;
-        cin >> payment;
-        CRDT.payInstallment(acc_num, PC, payment);
-        CRDT.showCreditState(acc_num, PC);
-        CRDT.payOffCredit(acc_num, PC);
-        CRDT.showCreditState(acc_num, PC);
-    } catch (const out_of_range& e) { // значение выходит за пределы допустимого диапазона
-        cerr << "Error: " << e.what() << endl;
-    } catch (const invalid_argument& e) { // аргумент недопустимого типа или значения
-        cerr << "Error: " << e.what() << endl;
-    } catch (const runtime_error& e) { // ошибка, возникающая во время выполнения программы
-        cerr << "Error: " << e.what() << endl;
+        memset(cells[height - 1], '#', width); // нижняя граница
     }
+
+    ~Field() // деструктор
+    {
+        // освобождение памяти от двумерного массива
+        for (int i = 0; i < height; i++)
+            delete[] cells[i];
+        delete[] cells;
+    }
+
+};
+
+enum Direction {LEFT, RIGHT, UP, DOWN }; // перечисление направлений движения
+
+class Snake
+{
+public:
+    int length; // длина змейки
+    Direction direction = LEFT; // направление движения змейки
+    vector<pair<int, int>> body; // вектор координат всех сегментов тела змейки (включая голову)
+    int speed; // скорость змейки
+
+    // ---------------------------------------------------------------------------------
+
+    Snake(int startX, int startY, int speed2) : length(5), direction(LEFT), speed(speed2) // конструктор-инициализатор
+    {
+        for (int i = 0; i < 5; i++) // заполняем вектор snake начальными координатами 5 сегментов змейки
+            body.push_back({ startX + i, startY }); // "хвост" расположен справа от "головы"
+    }
+
+    bool isInsideBody(int x, int y) // функция, проверяющая нахождение заданной точки внутри змейки
+    {
+        for (const auto& segment : body)  // range-based цикл для прохода по контейнеру vector
+            if (segment.first == x && segment.second == y)
+                return true;
+        return false;
+    }
+
+    void move() // функция, обновляющая позицию змейки
+    {
+        int newX = body[0].first;
+        int newY = body[0].second;
+        switch (direction) // изменяем координаты головы змейки в соответствии с направлением
+        {
+        case LEFT:
+            newX--;
+            break;
+        case RIGHT:
+            newX++;
+            break;
+        case UP:
+            newY--;
+            break;
+        case DOWN:
+            newY++;
+            break;
+        default:
+            break;
+        }
+
+        for (int i = length - 1; i > 0; i--) // сдвигаем все сегменты тела змейки, кроме головы
+            body[i] = body[i - 1];
+        body[0] = pair<int, int>(newX, newY); // обновление позиции головы змейки
+
+    }
+
+    bool checkCollision(const Field& field) // функция, проверяющая столкновение змейки с самой собой или стенами
+    {
+        int headX = body[0].first;
+        int headY = body[0].second;
+        if (headX < 1 || headX >= field.width - 1 || headY < 1 || headY >= field.height - 1) // проверка на столкновение со стенами
+            return true;
+        for (int i = 1; i < length; i++)
+            if (headX == body[i].first && headY == body[i].second) // проверка на столкновение змейки с самой собой
+                return true;
+        return false;
+    }
+
+    void grow() // функция, увеличивающая длину змейки
+    {
+        body.push_back(body.back()); // добавляем новый сегмент в конец тела змейки
+        length++;
+    }
+};
+
+class Food
+{
+public:
+    int fruitX;
+    int fruitY;
+
+    // ---------------------------------------------------------------------------------
+
+    Food(int fruitX2, int fruitY2) : fruitX(fruitX2), fruitY(fruitY2) {}
+};
+
+
+class Game
+{
+private:
+    Field field;
+    Snake snake;
+    Food food;
+    bool gameOver = false; // флаг об окончании игры
+    int winLength; // длина змейки для выигрыша
+public:
+    Game(int width2, int height2, int startX2, int startY2, int speed2,int fruitX2, int fruitY2, int winLength2) : field(width2, height2), snake(startX2, startY2, speed2), food(fruitX2, fruitY2), winLength(winLength2) {} // конструктор-инициализатор
+
+    void draw() // функция, выводящая в консоль игровое поле, змейку и фрукты
+    {
+        system("cls"); // очистка консоли
+        for (int i = 0; i < field.height; i++) 
+        {
+            for (int j = 0; j < field.width; j++)
+            {
+                bool isSnake = false; // флаг: клетка внутри змейки
+                bool isHead = false; // флаг : клетка - голова змейки
+                for (const auto& segment : snake.body) // range-based цикл для прохода по контейнеру vector
+                {
+                    if (segment.first == j && segment.second == i)
+                    {
+                        isSnake = true;
+                        if (segment.first == snake.body[0].first && segment.second == snake.body[0].second)
+                            isHead = true;
+                        break;
+                    }
+                }
+                bool isFruit = false; // флаг : клетка - фрукт
+                if (food.fruitX == j && food.fruitY == i)
+                    isFruit = true;
+                // вывод в консоль и окраска символов
+                if (isSnake && isHead)
+                {
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Цвет головы (синий)
+                    cout << 'O'; // символ для тела змейки
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Сброс цвета
+                }
+                else if (isSnake)
+                {
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);  // Цвет тела (голубой)
+                    cout << 'O'; // символ для тела змейки
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Сброс цвета
+                }
+                else if (isFruit)
+                {
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Цвет фрукта (зелёный)
+                    cout << '@'; // символ для фрукта
+                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // сброс цвета
+                }
+                else
+                    cout << field.cells[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    void Input() // функция, считывающая нажатия клавиш
+    {
+        if (_kbhit()) // если клавиша нажата
+        {
+            char key = _getch(); // что за клавиша нажата
+            switch (key)
+            {
+            case 'w': // "стрелка" вверх
+                if (snake.direction != DOWN)
+                    snake.direction = UP;
+                break;
+            case 's': // "стрелка" вниз
+                if (snake.direction != UP)
+                    snake.direction = DOWN;
+                break;
+            case 'a': // "стрелка" влево
+                if (snake.direction != RIGHT)
+                    snake.direction = LEFT;
+                break;
+            case 'd': // "стрелка" вправо
+                if (snake.direction != LEFT)
+                    snake.direction = RIGHT;
+                break;
+            }
+        }
+    }
+
+    void update() //  функция, обновляющая состояние игры
+    {
+        snake.move();
+        if (snake.body[0].first == food.fruitX && snake.body[0].second == food.fruitY) // если змейка съела фрукт
+        {
+            snake.grow();
+            do {
+                food.fruitX = rand() % (field.width - 2) + 1;
+                food.fruitY = rand() % (field.height - 2) + 1;
+            } while (snake.isInsideBody(foodX, foodY)); // пока фрукт спавнится внутри змейки
+        }
+    }
+
+    void gameplay() // функция, осуществляющая основной игровой процесс
+    {
+        while (!gameOver)
+        {
+            Input();
+            update();
+            draw();
+            if (snake.checkCollision(field)) // проверка на конец игры (значение переменной gameOver)
+            {
+                gameOver = true;
+                cout << "Snake length: " << snake.length << endl;
+                cout << "YOU HAVE LOST !" << endl;
+            }
+            if (snake.length >= winLength)
+            {
+                gameOver = true;
+                cout << "Snake length: " << snake.length << endl;
+                cout << "YOU HAVE WON !" << endl;
+            }
+            Sleep(snake.speed); // приостанавливает выполнение текущего потока (иллюзия скорости)
+        }
+    }
+};
+
+int main() {
+
+    srand(time(NULL)); // инициализация генератора случайных чисел
+    int M = 60;
+    int N = 20;
+    int headX = rand() % (M - 4) + 1; // ограничение для спавна змейки
+    if (headX < 5)
+        headX = 5; // запас перед левой границей поля (для более комфортного геймплея)
+    int headY = rand() % N + 1;
+    int foodX, foodY;
+    int snake_speed = 100;
+    int win = 20; // длина змейки для победы
+    Snake snake(headX, headY, snake_speed); // объект типа snake (нужен для правильной генерации первого фрукта)
+    do {
+        foodX = rand() % M + 1;
+        foodY = rand() % N + 1;
+    } while (snake.isInsideBody(foodX, foodY));
+    Game game(M, N, headX, headY, snake_speed, foodX, foodY, win);
+    game.gameplay();
     return 0;
 }
+
 
